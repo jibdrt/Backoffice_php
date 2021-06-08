6cb6b8f2824d8bfe6aba2f5a8f7e445578f8cbbf
JbDruet <jb.druet@codeur.online>
2021-06-08 13:34:27 +0200

    update

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" href="style.css">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="index-style.css">
     <title>Accueil</title>
 </head>
 <body>
-    <a href="register-form.php">S'inscrire</a>
-    <a href="login-form.php">Se connecter</a>
+<div class="bg">
+    <div class="wrapper">
+        <div class="box register">
+        <a href="register-form.php">REGISTER</a>
+        </div>
+
+        <div class="box login">
+        <a href="login-form.php">LOGIN</a>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
