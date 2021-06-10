c48c6542bd401e39d0b705c1346343d4e62d63af
JbDruet <jb.druet@codeur.online>
2021-06-10 16:55:52 +0200

    wave

--- indextry.php
+++ indextry.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet" href="style.css">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="indextry-style.css">
-    <title>Accueil</title>
+    <title>LOGIN</title>
 </head>
 <body>
-    <a href="register-form.php">
+    <a href="#">
         <span class="gap1">LOGIN</span>
         <div class="liquid"></div>
     </a>
 
-    <a href="login-form.php">
+    <a href="#">
         <span class="gap2">REGISTER</span>
         <div class="liquid"></div>
     </a>
