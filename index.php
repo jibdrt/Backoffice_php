1733c9c0684bb76c4eaa1b4c52c65aef76f5116b
JbDruet <jb.druet@codeur.online>
2021-07-27 16:56:44 +0200

    trial-anim-init

--- index.php
+++ index.php
@@ -25,19 +25,19 @@
   <nav class="menu__nav">
     <ul class="menu__list r-list">
       <li class="menu__group">
-        <a href="#0" class="menu__link r-link nav__link">
+        <a href="front/projects.php" class="menu__link r-link nav__link">
                         <span class="nav__link--text" data-splitting>PROJETS</span>
                         <span class="nav__link--text" data-splitting>PROJECTS</span>
         </a>
       </li>
       <li class="menu__group">
-        <a href="#0" class="menu__link r-link nav__link">
+        <a href="front/about.php" class="menu__link r-link nav__link">
                         <span class="nav__link--text" data-splitting>À PROPOS DE MOI</span>
                         <span class="nav__link--text" data-splitting>ABOUT</span>
         </a>
       </li>
       <li class="menu__group">
-        <a href="#0" class="menu__link r-link nav__link">
+        <a href="front/contactform.php" class="menu__link r-link nav__link">
                 <span class="nav__link--text" data-splitting>ME CONTACTER</span>
                 <span class="nav__link--text" data-splitting>CONTACT ME</span>
         </a>
